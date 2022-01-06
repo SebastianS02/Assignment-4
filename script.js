378e3deedd2d43d6d32111fc72053bbfbce2700b
SebastianS02 <stecsebastian2002@gmail.com>
2022-01-06 17:29:30 -0500

    final commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function clickMe(){
+    var a = document.getElementById("myDiv");
+    if(a.innerHTML === "No I'm Right"){
+        a.innerHTML = "I'm Right";
+    }
+}
+function noClickMe(){
+    var b = document.getElementById("myDiv");
+    if(b.innerHTML === "I'm Right"){
+        b.innerHTML = "No I'm Right";
+    }
+}
+
+//------------------------------------------------------
+function hoverAlert(){
+    alert("Don't hover over me");
+}
+
+//------------------------------------------------------
+function checkPass(){
+    let pass = document.querySelector('input').value;
+    let c = document.getElementById("myForm");
+    if(pass === '12345678'){
+        c.innerHTML = "Correct!";
+    }
+    else{
+        alert('The password is incorrect!');
+    }
+}
